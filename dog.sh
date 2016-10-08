df50cc4782b06b38cb060fe2b4779fe2149492e9
Hurricane Hamilton <hhamilto@mtu.edu>
2016-10-08 16:43:56 -0500

    Better now that it uses pids?

--- dog.sh
+++ dog.sh
@@ -2,23 +2,22 @@
 
 . conf.env
 
-if [ -z "$WATCHED_PROCESS_NAME" ] ; then
-	echo 'WATCHED_PROCESS_NAME not defined'
-	echo 'define WATCHED_PROCESS_NAME in conf.env'
+if [ -z "$RESTART_COMMAND" ] ; then
+	echo 'RESTART_COMMAND not defined'
+	echo 'define RESTART_COMMAND in conf.env'
 	exit 1
 fi
 
-echo '$WATCHED_PROCESS_NAME:' $WATCHED_PROCESS_NAME
 echo '$RESTART_COMMAND:' $RESTART_COMMAND
 
+nohup "$RESTART_COMMAND" &> /dev/null &
+WATCHED_PID=$!
+
 while true ; do
-	if [ `ps -eo cmd | grep '^'"$WATCHED_PROCESS_NAME" | wc -l` -lt 1 ] ; then
+	if [ `ps -eo pid | grep '^ '$WATCHED_PID'$' | wc -l` -lt 1 ] ; then
 		echo [ `date` ] 'Restarting Process'
-		echo [ `date` ] 'Number of processes currently alive: ' `ps -eo cmd:12 | grep '^'"$WATCHED_PROCESS_NAME" | wc -l`
 		nohup "$RESTART_COMMAND" &> /dev/null &
-	fi
-	if [ `ps -eo cmd | grep '^'"$WATCHED_PROCESS_NAME" | wc -l` -gt 1 ] ; then
-		echo [ `date` ] 'More than ONE process running'
+		WATCHED_PID=$!
 	fi
 	sleep 1
 done
