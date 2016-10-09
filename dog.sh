3bb8d98278f488ae83152c57bbea39f238f69b4f
Hurricane Hamilton <hhamilto@mtu.edu>
2016-10-08 20:37:06 -0500

    log application output, instead of /dev/null ing it

--- dog.sh
+++ dog.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+APP_LOG_FILE=app.log
+
 . conf.env
 
 if [ -z "$RESTART_COMMAND" ] ; then
@@ -10,13 +12,15 @@ fi
 
 echo '$RESTART_COMMAND:' $RESTART_COMMAND
 
-nohup "$RESTART_COMMAND" &> /dev/null &
+
+
+nohup "$RESTART_COMMAND" &> $APP_LOG_FILE &
 WATCHED_PID=$!
 
 while true ; do
 	if [ `ps -eo pid | grep '^ '$WATCHED_PID'$' | wc -l` -lt 1 ] ; then
 		echo [ `date` ] 'Restarting Process'
-		nohup "$RESTART_COMMAND" &> /dev/null &
+		nohup "$RESTART_COMMAND" &> $APP_LOG_FILE &
 		WATCHED_PID=$!
 	fi
 	sleep 1
